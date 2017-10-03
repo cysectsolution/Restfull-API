9f0595862f098ae3448ca0aa3860d000681888d4
GeorgeKihara <gegohcomp@gmail.com>
2017-10-03 22:40:25 +0300

    crud operations

---
+++ routes/tasks.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+//var db = mongojs('');
+
+//Get all tasks
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+//Get a single task
+router.get('/tasks/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if (err) {
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+//Save a task
+router.post('/task', function(req, res, next){
+    var task = req.body;
+    if (!task.title || (task.isDone + '')){
+        res.status(400);
+        res.json({
+            "error" : "Data is not valid"
+        });
+    }else{
+        db.tasks.save(task, function(err, task){
+            if (err){
+                res.send(err)''
+            }
+            res.json(task);
+        });
+    }
+});
+
+//Delete a task
+router.delete('/task/:id', function(req, res, next){
+    db.task.remove({_id:mongojs.ObjectId(req.params.id)}, function(err, task){
+        if (err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+//update a task
+router.put('/task/:id', function(req, res, next){
+    var task = req.body;
+    var upTask = {};
+    if (task.isDone){
+        upTask.isDone = task.isDone;
+    }
+    if (task.title){
+        upTask.title = task.title;
+    }
+    if (!upTask){
+        res.status(400);
+        res.json({
+            "error" : "Bad data"
+        });
+    }else{
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+            if (err){
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
+});
+
+module.exports = router;
